55aa587b2921e063e8a7f340d190e968664fba7f
Sol Ochs <106677464+sol-ochs@users.noreply.github.com>
2026-01-31 13:12:10 -0500

    Bump target SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.aurox.gotham"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.aurox.gotham"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
 
