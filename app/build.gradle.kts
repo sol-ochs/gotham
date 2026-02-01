1418a66f11408eedae6afe09d2b3e8af1e156e24
Sol Ochs <106677464+sol-ochs@users.noreply.github.com>
2026-02-01 14:03:02 -0500

    Bump version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.aurox.gotham"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
