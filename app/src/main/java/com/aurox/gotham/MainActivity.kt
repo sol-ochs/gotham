3c5b832ac64fd8d506757e5210546346b81bd3f7
Sol Ochs <106677464+sol-ochs@users.noreply.github.com>
2026-02-01 16:39:32 -0500

    Fix double-padding issue

--- app/src/main/java/com/aurox/gotham/MainActivity.kt
+++ app/src/main/java/com/aurox/gotham/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -35,6 +36,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         requestNotificationPermission()
 
--- app/src/main/java/com/aurox/gotham/presentation/theme/Theme.kt
+++ app/src/main/java/com/aurox/gotham/presentation/theme/Theme.kt
@@ -5,7 +5,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -44,7 +43,6 @@ fun GothamTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = DarkColorScheme.background.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
         }
     }
