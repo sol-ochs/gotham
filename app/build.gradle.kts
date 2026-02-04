acb87cf77dccb318dbf561be217275f94f95429d
Sol Ochs <106677464+sol-ochs@users.noreply.github.com>
2026-02-04 10:05:55 -0500

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.aurox.gotham"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.0.2"
+        versionCode = 6
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
