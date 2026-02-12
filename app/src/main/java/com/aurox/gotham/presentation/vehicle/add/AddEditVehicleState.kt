ec8d145c7f89e97dcac73f5b6353dabdb548175a
Sol Ochs <106677464+sol-ochs@users.noreply.github.com>
2026-02-12 13:23:23 -0500

    Drop vehicle type from vehicle flow

--- app/src/main/java/com/aurox/gotham/presentation/vehicle/add/AddEditVehicleState.kt
+++ app/src/main/java/com/aurox/gotham/presentation/vehicle/add/AddEditVehicleState.kt
@@ -1,14 +1,12 @@
 package com.aurox.gotham.presentation.vehicle.add
 
 import com.aurox.gotham.domain.model.UsState
-import com.aurox.gotham.domain.model.VehicleType
 
 data class AddEditVehicleState(
     val vehicleId: Long? = null,
     val licensePlate: String = "",
     val nickname: String = "",
     val selectedState: UsState = UsState.NY,
-    val selectedVehicleType: VehicleType = VehicleType.UNSPECIFIED,
     val isLoading: Boolean = false,
     val error: String? = null,
     val isSaved: Boolean = false,
